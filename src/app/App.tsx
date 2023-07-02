c8f4a2e70dd804a199e08dc32f567ce118594ed3
DarkVito <andreikreslo@mail.ru>
2023-07-02 13:28:28 +0400

    Sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,16 +3,18 @@ import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from 'app/providers/ThemeProvider'
 import { AppRouter } from './providers/Router'
 import { Navbar } from 'widgets/Navbar'
+import { Sidebar } from 'widgets/Sidebar'
 
 
 const App = () => {
     const { theme } = useTheme()
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar/>
-            <AppRouter />
-
-            <AppRouter />
+            <Navbar />
+            <div className='content-page'>
+                <Sidebar />
+                <AppRouter />
+            </div>
         </div>
     )
 }
